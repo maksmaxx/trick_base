b6fa26a5233555fe566182fcc3efe3734d84a9c2
maksmaxx <36051628+maksmaxx@users.noreply.github.com>
2020-08-06 17:40:19 +0200

    Adds basic routes.

--- api/api.py
+++ api/api.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_restful import Api
+
 from resources.discipline import Discipline
+from resources.discipline_list import DisciplineList
+from resources.trick import Trick
+from resources.trick_list import TrickList
 
 app = Flask(__name__)
 api = Api(app)
 
 # Routes
-api.add_resource(Discipline, "/disciplines/<string:name>")
+api.add_resource(DisciplineList, "/api/disciplines")                # Returns all disciplines
+api.add_resource(Discipline, "/api/disciplines/<string:name>")      # Returns discipline
+api.add_resource(TrickList, "/api/tricks/<string:discipline>")      # Returns all tricks of 1 discipline
+api.add_resource(Trick, "/api/trick/<string:id>")                   # Returns trick
 
 if __name__ == '__main__':
     # TODO - DELETE DEBUG
